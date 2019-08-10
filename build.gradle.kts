7af5db45b04a9530e65c05629f2ab1def6279a49
doyaaaaaken <kenta.koyama.biz@gmail.com>
2019-08-10 13:02:20 +0900

    introduce maven-publish plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,10 @@
 plugins {
     id("org.jetbrains.kotlin.jvm").version("1.3.41")
+    `maven-publish`
 }
+
+group = "org.doyaaaaaken.kotlincsv"
+version = "0.1.0-SNAPSHOT"
 
 repositories {
     jcenter()
@@ -10,4 +14,17 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     testImplementation("org.jetbrains.kotlin:kotlin-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("myLibrary") {
+            from(components["java"])
+        }
+    }
+    repositories {
+        maven {
+            url = uri("file://Users/kenta/workspace/oss/kotlin-csv")
+        }
+    }
 }
