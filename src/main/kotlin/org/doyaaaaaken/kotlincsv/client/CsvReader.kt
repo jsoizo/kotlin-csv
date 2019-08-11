65c873e7d0237f19258d47fd011c455318a5bba9
doyaaaaaken <kenta.koyama.biz@gmail.com>
2019-08-11 13:16:55 +0900

    implement read method

--- src/main/kotlin/org/doyaaaaaken/kotlincsv/client/CsvReader.kt
+++ src/main/kotlin/org/doyaaaaaken/kotlincsv/client/CsvReader.kt
@@ -2,14 +2,34 @@ package org.doyaaaaaken.kotlincsv.client
 
 import org.doyaaaaaken.kotlincsv.dsl.context.CsvReaderContext
 import org.doyaaaaaken.kotlincsv.dsl.context.ICsvReaderContext
+import java.io.BufferedReader
+import java.io.File
+import kotlin.streams.asSequence
 
 class CsvReader(ctx: CsvReaderContext = CsvReaderContext()) : ICsvReaderContext by ctx {
 
-    fun read(data: String): Sequence<List<String>> {
-        println(data)
-        return listOf(
-                listOf("a1", "a2", "a3"),
-                listOf("b1", "b2", "b3")
-        ).asSequence()
+    fun read(data: String): List<List<String>> {
+        return readAsSequence(data).toList()
+    }
+
+    fun read(file: File): List<List<String>> {
+        return readAsSequence(file).toList()
+    }
+
+    fun readAsSequence(data: String): Sequence<List<String>> {
+        val br = data.byteInputStream(charset).bufferedReader(charset)
+        return readWithBufferedReader(br)
+    }
+
+    fun readAsSequence(file: File): Sequence<List<String>> {
+        val br = file.inputStream().bufferedReader(charset)
+        return readWithBufferedReader(br)
+    }
+
+    private fun readWithBufferedReader(br: BufferedReader): Sequence<List<String>> {
+        return br.lines().asSequence().map {
+            //TODO: implement parse logic
+            it.split(",")
+        }
     }
 }
--- src/test/kotlin/org/doyaaaaaken/kotlincsv/client/CsvReaderTest.kt
+++ src/test/kotlin/org/doyaaaaaken/kotlincsv/client/CsvReaderTest.kt
@@ -1,24 +1,39 @@
 package org.doyaaaaaken.kotlincsv.client
 
 import io.kotlintest.shouldBe
-import io.kotlintest.specs.StringSpec
+import io.kotlintest.specs.WordSpec
 import org.doyaaaaaken.kotlincsv.dsl.context.CsvReaderContext
 import org.doyaaaaaken.kotlincsv.util.Const
 
-class CsvReaderTest : StringSpec({
-    "CsvReader should be created by class constructor with no argument" {
-        val reader = CsvReader()
-        reader.charset shouldBe Const.defaultCharset
-    }
-    "CsvReader should be created by class constructor with CsvReaderContext argument" {
-        val context = CsvReaderContext().apply {
-            charset = Charsets.ISO_8859_1
-            delimiter = '\t'
-            withHeader = false
+class CsvReaderTest : WordSpec({
+
+    "CsvReader class constructor" should {
+        "be created with no argument" {
+            val reader = CsvReader()
+            reader.charset shouldBe Const.defaultCharset
         }
-        val reader = CsvReader(context)
-        reader.charset shouldBe Charsets.ISO_8859_1
-        reader.delimiter shouldBe '\t'
-        reader.withHeader shouldBe false
+        "be created with CsvReaderContext argument" {
+            val context = CsvReaderContext().apply {
+                charset = Charsets.ISO_8859_1
+                delimiter = '\t'
+                withHeader = false
+            }
+            val reader = CsvReader(context)
+            reader.charset shouldBe Charsets.ISO_8859_1
+            reader.delimiter shouldBe '\t'
+            reader.withHeader shouldBe false
+        }
+    }
+
+    "read method (with String argument)" should {
+        //TODO: implement about various type of csv files
+        "read csv" {
+            val result = CsvReader().read(
+                    """a,b,c
+                        |d,e,f
+                    """.trimMargin()
+            )
+            result shouldBe listOf(listOf("a", "b", "c"), listOf("d", "e", "f"))
+        }
     }
 })
