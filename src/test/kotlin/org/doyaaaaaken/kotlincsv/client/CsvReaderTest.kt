55b05368344d73fb1f523d709b0c18048502bae0
doyaaaaaken <kenta.koyama.biz@gmail.com>
2019-08-11 13:40:41 +0900

    Add File read test

--- src/test/kotlin/org/doyaaaaaken/kotlincsv/client/CsvReaderTest.kt
+++ src/test/kotlin/org/doyaaaaaken/kotlincsv/client/CsvReaderTest.kt
@@ -4,36 +4,51 @@ import io.kotlintest.shouldBe
 import io.kotlintest.specs.WordSpec
 import org.doyaaaaaken.kotlincsv.dsl.context.CsvReaderContext
 import org.doyaaaaaken.kotlincsv.util.Const
+import java.io.File
 
-class CsvReaderTest : WordSpec({
+class CsvReaderTest : WordSpec() {
 
-    "CsvReader class constructor" should {
-        "be created with no argument" {
-            val reader = CsvReader()
-            reader.charset shouldBe Const.defaultCharset
-        }
-        "be created with CsvReaderContext argument" {
-            val context = CsvReaderContext().apply {
-                charset = Charsets.ISO_8859_1
-                delimiter = '\t'
-                withHeader = false
+    init {
+        "CsvReader class constructor" should {
+            "be created with no argument" {
+                val reader = CsvReader()
+                reader.charset shouldBe Const.defaultCharset
             }
-            val reader = CsvReader(context)
-            reader.charset shouldBe Charsets.ISO_8859_1
-            reader.delimiter shouldBe '\t'
-            reader.withHeader shouldBe false
+            "be created with CsvReaderContext argument" {
+                val context = CsvReaderContext().apply {
+                    charset = Charsets.ISO_8859_1
+                    delimiter = '\t'
+                    withHeader = false
+                }
+                val reader = CsvReader(context)
+                reader.charset shouldBe Charsets.ISO_8859_1
+                reader.delimiter shouldBe '\t'
+                reader.withHeader shouldBe false
+            }
         }
-    }
 
-    "read method (with String argument)" should {
-        //TODO: implement about various type of csv files
-        "read csv" {
-            val result = CsvReader().read(
-                    """a,b,c
+        "read method (with String argument)" should {
+            //TODO: implement about various type of csv files
+            "read csv" {
+                val result = CsvReader().read(
+                        """a,b,c
                         |d,e,f
                     """.trimMargin()
-            )
-            result shouldBe listOf(listOf("a", "b", "c"), listOf("d", "e", "f"))
+                )
+                result shouldBe listOf(listOf("a", "b", "c"), listOf("d", "e", "f"))
+            }
+        }
+
+        "read method (with File argument)" should {
+            //TODO: implement about various type of csv files
+            "read csv" {
+                val result = CsvReader().read(readTestDataFile("simple.csv"))
+                result shouldBe listOf(listOf("a", "b", "c"), listOf("d", "e", "f"))
+            }
         }
     }
-})
+
+    private fun readTestDataFile(fileName: String): File {
+        return File("src/test/resources/testdata/csv/$fileName")
+    }
+}
