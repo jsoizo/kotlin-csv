72b4c8b01f1770a0506666bf35476eff5f8f60b8
doyaaaaaken <kenta.koyama.biz@gmail.com>
2019-09-22 23:17:05 +0900

    refs #12 add failed test

--- src/jvmTest/kotlin/com/github/doyaaaaaken/kotlincsv/client/CsvReaderTest.kt
+++ src/jvmTest/kotlin/com/github/doyaaaaaken/kotlincsv/client/CsvReaderTest.kt
@@ -138,6 +138,18 @@ class CsvReaderTest : WordSpec() {
                 val result = csvReader().readAll(readTestDataFile("unicode2028.csv"))
                 result shouldBe listOf(listOf("\u2028"))
             }
+            "read csv with empty lines" {
+                val result = csvReader {
+                    skipEmptyLine = true
+                }.readAll(readTestDataFile("empty-line.csv"))
+                result shouldBe listOf(listOf("a", "b", "c"), listOf("d", "e", "f"))
+            }
+            "read csv with quoted empty line field" {
+                val result = csvReader {
+                    skipEmptyLine = true
+                }.readAll(readTestDataFile("quoted-empty-line.csv"))
+                result shouldBe listOf(listOf("a", "b", "c\n\nc"), listOf("d", "e", "f"))
+            }
             "throw exception when reading malformed csv" {
                 shouldThrow<MalformedCSVException> {
                     csvReader().readAll(readTestDataFile("malformed.csv"))
